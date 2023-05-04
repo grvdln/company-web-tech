a6692e422a0ffbd0ebbd4d4c9f964d91d36521c4
gaurav <grvdln90@gmail.com>
2023-05-04 20:24:13 +0530

    added newsletter

---
+++ src/components/Experts.js
@@ -0,0 +1,31 @@
+import laptop from "../assets/laptop.jpg";
+import React from "react";
+
+function Experts() {
+  return (
+    <div className="max-w-[1240px] mx-auto my-10 p-2 md:grid grid-cols-2">
+      <div className="md:w-[80%]">
+        <img src={laptop} alt="laptop" />
+      </div>
+      <div className="flex flex-col justify-center">
+        <h1 className="text-[#00df9a] font-bold my-2">LEARN FROM EXPERTS</h1>
+        <p className="my-2 text-justify">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+        <button className="w-[30%] bg-black text-white p-3 rounded">
+          Get started
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Experts;
---
+++ src/components/NewsLetter.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function NewsLetter() {
+  return (
+    <div className="bg-[#2699fb] p-4">
+      <div className="max-w-[1240px] md:flex justify-between mx-auto py-[50px]">
+        <div className="m-2">
+          <div className="text-white text-[20px] md:text-[40px] font-bold]">
+            Want to learn latest I.T skills?
+          </div>
+          <span className="text-white ">
+            Sign up to our newletter and staty up to date.
+          </span>
+        </div>
+        <div className="m-1" >
+          <input
+            className="p-3 mr-2 rounded mb-2 text-slate-600"
+            type="email"
+            placeholder="Enter Email"
+          />
+          <button className="w-[30%] bg-black text-white p-3 rounded">
+            Get started
+          </button>
+          <br />
+          <p className="text-white">
+            We care about the protection of your data. Read our <br />{" "}
+            <a className="text-black">Privacy policy</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NewsLetter;
